54ffbb05a8a0370a0c2ed368f0f663cbe6f86a76
AndresMpa <andresprieto2703@gmail.com>
2020-12-18 00:25:15 -0500

    base

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import Vue from "vue";
+import axios from "axios";
+import App from "./App.vue";
+import router from "./router";
+
+const base = axios.create({
+  baseURL: "http://localhost:3000/"
+});
+
+Vue.prototype.$http = base;
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount("#app");
---
+++ src/views/AdminView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <list-items></list-items>
+  </div>
+</template>
+
+<script>
+import ListItems from "../components/ListItems.vue";
+
+export default {
+  name: "ListItems",
+  components: {
+    ListItems
+  }
+};
+</script>
